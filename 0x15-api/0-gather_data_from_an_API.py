db52cc8db46dbf770a88608a5dd4e8417ad02554
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-04-25 21:48:00 +0100

    added a call guard

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,30 +3,30 @@
 import requests
 import sys
 
-TOTAL_NUMBER_OF_TASKS = 0
-NUMBER_OF_DONE_TASKS = 0
+if __name__ == "__main__":
+	"""function entry to avoid issues"""
+	TOTAL_NUMBER_OF_TASKS = 0
+	NUMBER_OF_DONE_TASKS = 0
 
-if len(sys.argv) == 2:
-    employeeID = int(sys.argv[1])
-else:
-    print("input integer")
-users = requests.get('https://jsonplaceholder.typicode.com/users')
-employee = users.json()
+	employeeID = int(sys.argv[1])
 
-"""get employee name"""
-for i in employee:
-    if i["id"] == employeeID:
-        EMPLOYEE_NAME = i["name"]
+	users = requests.get('https://jsonplaceholder.typicode.com/users')
+	employee = users.json()
 
-"""print todos"""
-response = requests.get(
-    f'https://jsonplaceholder.typicode.com/todos/{employeeID}/todos')
-TODO = response.json()
-for entry in TODO:
-    TOTAL_NUMBER_OF_TASKS += 1
-    if entry["completed"] is True:
-        NUMBER_OF_DONE_TASKS += 1
-print(f"Employee {EMPLOYEE_NAME} is done with tasks" +
-      "({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-for entry in TODO:
-    print("\t " + entry.get("title"))
+	"""get employee name"""
+	for i in employee:
+		if i["id"] == employeeID:
+			EMPLOYEE_NAME = i["name"]
+
+	"""print todos"""
+	response = requests.get(
+		f'https://jsonplaceholder.typicode.com/todos/{employeeID}/todos')
+	TODO = response.json()
+	for entry in TODO:
+		TOTAL_NUMBER_OF_TASKS += 1
+		if entry["completed"] is True:
+			NUMBER_OF_DONE_TASKS += 1
+	print(f"Employee {EMPLOYEE_NAME} is done with tasks" +
+		f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+	for entry in TODO:
+		print("\t " + entry.get("title"))
