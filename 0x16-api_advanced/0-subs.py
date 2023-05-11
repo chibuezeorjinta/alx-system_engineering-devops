2408b1af05604f1a3e7771a5bd546494818ec860
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-05-11 19:57:27 +0100

    manipulate reddit api to get current hot topics

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Get subscriber count from reddit"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit.
+        Args:
+            subreddit (str): subreddit in question
+            """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Emergency-Jaguar3819)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Function to print hot posts on a given Reddit subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """Get to ten hot posts on a subreddit.
+        Args:
+            subreddit (str): queried subreddit
+            """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    params = {
+        "limit": 10
+    }
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Emergency-Jaguar3819)"
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    for c in results.get("children"):
+        print(c.get("data").get("title"))
