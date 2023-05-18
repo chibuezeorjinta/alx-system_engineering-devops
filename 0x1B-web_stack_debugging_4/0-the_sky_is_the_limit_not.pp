d73574f73b7dda92b593598e2b36c283b1f75e94
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-05-18 16:13:05 +0100

    puppet lint edits

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,12 @@
-# Increase max open files
-exec {'change':
-  provider  =>  shell,
-  command   =>  'sudo sed -i 's\ULIMIT="-n 15"\ULIMIT="-n 4096"\' /etc/default/nginx',
-  before    =>  Exec['nginx_restart']}
+# CHANGE THE AMOUNT OF OPEN FILES FROM NGINX DEFAULT FILE
 
 exec {'nginx_restart':
-  provider  =>  shell,
-  command   =>  'sudo service nginx restart'
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
+
+exec {'ULIMIT_EDIT':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['nginx_restart'],
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,12 @@
-# Increase files for holberton user
-exec {'edit':
-  provider  =>  shell,
-  command   =>  'sudo sed -i 's\^holberton hard nofile 5\holberton hard nofile 4000\' /etc/security/limits.conf',
-  command   =>  'sudo sed -i 's\^holberton soft nofile 4\holberton soft nofile 4000\' /etc/security/limits.conf',
+# Increase the system amount of files that can be opened by a user
+
+exec {'hard':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['soft'],
+}
+
+exec {'soft':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
